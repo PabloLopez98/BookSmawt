2897027291aa6a029e08131026dacfc80162f693
PabloLopez1998 <35881455+PabloLopez1998@users.noreply.github.com>
2020-02-14 04:39:19 -0800

    Up Next: Filter Search Screen UI

--- app/src/main/java/pablo/myexample/booksmawt/search/SearchFragment.kt
+++ app/src/main/java/pablo/myexample/booksmawt/search/SearchFragment.kt
@@ -7,10 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.activity_base.*
 import pablo.myexample.booksmawt.R
 
