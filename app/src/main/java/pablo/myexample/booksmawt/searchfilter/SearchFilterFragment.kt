56404f58dc225b4e3de854ea8ba7fd57b3ed7330
PabloLopez1998 <35881455+PabloLopez1998@users.noreply.github.com>
2020-02-15 00:42:29 -0800

    Up Next: SearchView Search Results and Search Content

--- app/src/main/java/pablo/myexample/booksmawt/searchfilter/SearchFilterFragment.kt
+++ app/src/main/java/pablo/myexample/booksmawt/searchfilter/SearchFilterFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toolbar
 import androidx.fragment.app.FragmentActivity
+import com.crystal.crystalrangeseekbar.widgets.CrystalRangeSeekbar
 import kotlinx.android.synthetic.main.activity_base.*
 
 import pablo.myexample.booksmawt.R
@@ -22,11 +24,23 @@ class SearchFilterFragment : Fragment() {
         return inflater.inflate(R.layout.search_filter_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val rangSB = view.findViewById<CrystalRangeSeekbar>(R.id.search_filter_seekbar)
+        val minTv = view.findViewById<TextView>(R.id.minimum)
+        val maxTv = view.findViewById<TextView>(R.id.maximum)
+        rangSB.setOnRangeSeekbarChangeListener { minValue, maxValue ->
+            minTv.text = minValue.toString()
+            maxTv.text = maxValue.toString()
+        }
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
     }
 
-    //in case user uses backpress key instead of buttons(apply or cancel), bottom nav in activity becomes visible
+    //in case user uses backpress key instead of buttons(apply or cancel),
+    //bottom nav in activity becomes visible
     override fun onDestroyView() {
         super.onDestroyView()
         activity!!.bottom_nav_view.visibility = View.VISIBLE
