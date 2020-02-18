8eddf02846c39ca70f20ba964a8ce7a278503f47
PabloLopez1998 <35881455+PabloLopez1998@users.noreply.github.com>
2020-02-17 20:29:18 -0800

    Up Next: Book Details

--- app/src/main/java/pablo/myexample/booksmawt/search/SearchFragment.kt
+++ app/src/main/java/pablo/myexample/booksmawt/search/SearchFragment.kt
@@ -1,15 +1,18 @@
 package pablo.myexample.booksmawt.search
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.activity_base.*
+import pablo.myexample.booksmawt.BookDetails.BookDetails
 import pablo.myexample.booksmawt.R
 
 
@@ -29,6 +32,10 @@ class SearchFragment : Fragment() {
             activity!!.bottom_nav_view.visibility = View.INVISIBLE
             //call fragment switch action
             view.findNavController().navigate(R.id.action_navigation_search_to_searchFilterFragment)
+        }
+        view.findViewById<ConstraintLayout>(R.id.constraint_layout).setOnClickListener {
+            activity!!.bottom_nav_view.visibility = View.INVISIBLE
+            view.findNavController().navigate(R.id.action_navigation_search_to_bookDetails)
         }
     }
 
