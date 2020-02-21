43ecd204d3677b393d7fa5b55dcc861a7fb29c7f
PabloLopez1998 <35881455+PabloLopez1998@users.noreply.github.com>
2020-02-20 20:45:44 -0800

    Up Next: Firebase Structure

--- app/src/main/java/pablo/myexample/booksmawt/list/ListFragment.kt
+++ app/src/main/java/pablo/myexample/booksmawt/list/ListFragment.kt
@@ -2,10 +2,13 @@ package pablo.myexample.booksmawt.list
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.text.Layout
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_base.*
 
 import pablo.myexample.booksmawt.R
 
@@ -22,6 +25,14 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.list_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<View>(R.id.include4).setOnClickListener {
+            activity!!.bottom_nav_view.visibility = View.INVISIBLE
+            view.findNavController().navigate(R.id.action_navigation_list_to_addedBookFragment)
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
