1d5dd895a2899c3afe531a23149fc91f2efa00b7
PabloLopez1998 <35881455+PabloLopez1998@users.noreply.github.com>
2020-03-11 23:09:32 -0700

    Up Next: Chat Adapter

--- app/src/main/java/pablo/myexample/booksmawt/chat/ChatFragment.kt
+++ app/src/main/java/pablo/myexample/booksmawt/chat/ChatFragment.kt
@@ -1,34 +1,87 @@
 package pablo.myexample.booksmawt.chat
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_base.*
+import pablo.myexample.booksmawt.*
+import pablo.myexample.booksmawt.databinding.ChatFragmentBinding
+import java.text.SimpleDateFormat
 
-import pablo.myexample.booksmawt.R
 
 class ChatFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ChatFragment()
-    }
-
-    private lateinit var viewModel: ChatViewModel
+    private lateinit var profileObj: Profile
+    private lateinit var binding: ChatFragmentBinding
+    private lateinit var model: Communicator
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.chat_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.chat_fragment, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ChatViewModel::class.java)
-        // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        model = ViewModelProvider(activity!!).get(Communicator::class.java)
+        model.bookObj.observe(activity!!, Observer<Book> { o ->
+            binding.bookObj = o
+            Picasso.get().load(o.urlList[0]).into(binding.chatFragImage)
+        })
+
+        getProfileData()
+
+        binding.chatFragSend.setOnClickListener {
+            checkInput()
+        }
+    }
+
+    private fun getProfileData() {
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
+        FirebaseDatabase.getInstance().reference.child("Users").child(userId).child("Profile")
+            .addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onCancelled(snapshotError: DatabaseError) {
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    when {
+                        snapshot.exists() -> {
+                            profileObj = snapshot.getValue(Profile::class.java)!!
+                        }
+                    }
+                }
+            })
+    }
+
+    private fun checkInput(){
+        when{
+            binding.chatFragInput.text.isBlank() -> Toast.makeText(context, "Input is empty", Toast.LENGTH_SHORT).show()
+            else -> sendMessage()
+        }
+    }
+
+    private fun sendMessage() {
+        //val message = Message(profileObj.name, "", "")
     }
 
     override fun onDestroyView() {
